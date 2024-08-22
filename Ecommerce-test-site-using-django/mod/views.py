7ebd12bd8e41161d6f34c15930193a67c0124360
Monofen <krishadhikari098@gmail.com>
2024-08-22 12:51:29 +0545

    user delete functio with reason and mail

--- Ecommerce-test-site-using-django/mod/views.py
+++ Ecommerce-test-site-using-django/mod/views.py
@@ -4,6 +4,8 @@ from authentication.models import UserProfile
 from sellers.models import Sellers
 from django.db.models import Q
 import hashlib
+from django.core.mail import send_mail, EmailMessage
+from django.conf import settings
 
 def admin_menu(request):
     return render(request, 'mod/admin_menu.html')
@@ -21,10 +23,24 @@ def manage_users(request):
 
     return render(request, 'mod/manage_users.html', {'users': users})
 
-
 def delete_user(request, user_id):
     user = get_object_or_404(User, id=user_id)
+    
     if request.method == 'POST':
+        reason = request.POST.get('reason')
+        custom_reason = request.POST.get('custom_reason', '')
+        
+        if reason == 'custom':
+            reason = custom_reason
+
+        subject = "Your profile has been deleted."
+        message = f"Hello {user.first_name},\n\nYour profile has been deleted from our site for the following reason:\n\n{reason}\n\nIf you believe this is a mistake, please contact our support team."
+
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [user.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=False)
+        
         user.delete()
         return redirect('manage_users')
+
     return redirect('manage_users')
